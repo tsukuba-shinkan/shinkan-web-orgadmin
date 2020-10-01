92d842d6f187c28e821550694eadca982b4ece3b
Kazumi Inada <hello@nandenjin.com>
2020-10-01 23:27:33 +0900

    chore: Linter/Formatterの導入 (#2)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'react',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'import/order': 'error',
+  }
+}
